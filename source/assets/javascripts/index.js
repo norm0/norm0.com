6539f38e3dedbd630799227bb23e056f3399d6d5
Norm Orstad <norman@orstad.com>
2025-02-12 01:59:42 -0600

    updated emory page

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,1 +1,21 @@
 import 'bootstrap';
+
+// Include Lightbox 
+import PhotoSwipeLightbox from 'photoswipe/lightbox';
+import PhotoSwipe from 'photoswipe';
+import 'photoswipe/photoswipe.css';
+
+// Initialize PhotoSwipe Lightbox
+const lightbox = new PhotoSwipeLightbox({
+	gallery: '.gallery', // Target the gallery div
+	children: 'a',       // Select all <a> elements inside the gallery
+	pswpModule: () => import('photoswipe'), // Dynamically load PhotoSwipe
+	showHideAnimationType: 'fade', // Animations: 'zoom', 'fade', 'none'
+	wheelToZoom: true,  // Enable zooming with the scroll wheel
+	loop: true,         // Enable infinite looping
+
+	getCaptionText: (slide) => slide.data.caption || '', // Fetch captions from `data-caption`
+});
+
+// Start the lightbox
+lightbox.init();
